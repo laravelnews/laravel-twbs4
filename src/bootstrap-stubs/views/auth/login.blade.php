fdfc4a5a8c7496ec0a538ca26ccd0cb5e3b7d9fd
Paul Redmond <paulrredmond@gmail.com>
2017-11-06 20:16:08 -0700

    Fix validation styling to use TWBS4 Classes

--- src/bootstrap-stubs/views/auth/login.blade.php
+++ src/bootstrap-stubs/views/auth/login.blade.php
@@ -10,30 +10,44 @@
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
-                        <div class="form-group row{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-right">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                                <input
+                                        id="email"
+                                        type="email"
+                                        class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                                        name="email"
+                                        value="{{ old('email') }}"
+                                        required
+                                        autofocus
+                                >
 
                                 @if ($errors->has('email'))
-                                    <span class="help-block">
-                                    <strong>{{ $errors->first('email') }}</strong>
-                                </span>
+                                    <div class="invalid-feedback">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </div>
                                 @endif
                             </div>
                         </div>
 
-                        <div class="form-group row{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-right">Password</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                <input
+                                        id="password"
+                                        type="password"
+                                        class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                                        name="password"
+                                        required
+                                >
 
                                 @if ($errors->has('password'))
-                                    <span class="help-block">
-                                    <strong>{{ $errors->first('password') }}</strong>
-                                </span>
+                                    <div class="invalid-feedback">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </div>
                                 @endif
                             </div>
                         </div>
