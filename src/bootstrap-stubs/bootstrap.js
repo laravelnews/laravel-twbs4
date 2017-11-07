5c522847119754b1022b2fafabc7ddd8bec12b7a
Paul Redmond <paulrredmond@gmail.com>
2017-11-06 19:46:07 -0700

    Add Popper.js

--- src/bootstrap-stubs/bootstrap.js
+++ src/bootstrap-stubs/bootstrap.js
@@ -1,6 +1,8 @@
 
 window._ = require('lodash');
 
+window.Popper = require('popper.js/dist/umd/popper');
+
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
