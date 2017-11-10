1e89f61adb57ebb6f2815e859a8602229451610d
Paul Redmond <paulrredmond@gmail.com>
2017-11-10 14:16:12 -0700

    Require jQuery slim instead of the full library (#13)

--- src/bootstrap-stubs/bootstrap.js
+++ src/bootstrap-stubs/bootstrap.js
@@ -10,7 +10,7 @@ window.Popper = require('popper.js/dist/umd/popper');
  */
 
 try {
-    window.$ = window.jQuery = require('jquery');
+    window.$ = window.jQuery = require('jquery/dist/jquery.slim');
 
     require('bootstrap');
 } catch (e) {}
